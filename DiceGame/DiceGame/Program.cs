dd46b9a3b8d21fab2552625879b2f2a7d28c2169
AisteL <aiste.lebedeva@gmail.com>
2018-12-15 23:00:14 +0200

    Base for the dice game

---
+++ DiceGame/DiceGame/Program.cs
@@ -0,0 +1,24 @@
+﻿using DiceGame.GUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            GuiController guiController = new GuiController();
+            //GameController gameController = new GameController();
+            //gameController.StartGame();
+            guiController.ShowMenu();
+
+            Console.ReadKey();
+
+        }
+    }
+}
